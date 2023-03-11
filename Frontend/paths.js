da5279f8cf1764df8acfaa1484037b1c6ed7dcc2
M17ggie <rmhatre799@gmail.com>
2023-03-11 22:35:55 +0530

    configured tsconfig.json and added path

---
+++ Frontend/paths.js
@@ -0,0 +1,9 @@
+import path from 'path';
+
+const aliases = {
+    '@components': path.resolve(__dirname, 'src/components'),
+    '@styles': path.resolve(__dirname, 'src/styles'),
+    '@utils': path.resolve(__dirname, 'src/utils')
+}
+
+export default aliases;
