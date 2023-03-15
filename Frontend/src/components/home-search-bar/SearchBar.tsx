026610eb9fcce136a5c93a64b4e2ebc1436e0d86
M17ggie <rmhatre799@gmail.com>
2023-03-15 21:52:06 +0530

    added navbar

--- Frontend/src/components/home-search-bar/SearchBar.tsx
+++ Frontend/src/components/home-search-bar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material'
+import Typography from '@mui/material/Typography'
 import styles from './SearchBar.module.css'
 
 const SearchBar = () => {
@@ -6,10 +6,10 @@ const SearchBar = () => {
         <>
             <div className="container">
                 <div className={styles['image-container']}>
-                    <Typography sx={{ zIndex: 1, color: 'white', fontSize: { xs: '2rem', md: '3.5rem' } }}>
+                    <Typography sx={{ zIndex: 1, color: 'white', fontSize: { xs: '2rem', md: '3rem' } }}>
                         Food Cab
                     </Typography>
-                    <Typography sx={{ zIndex: 1, color: 'white', fontSize: { xs: '1rem', sm: '1.5rem', md: '2.25rem' } }}>
+                    <Typography sx={{ zIndex: 1, color: 'white', fontSize: { xs: '1rem', sm: '1.5rem', md: '2.5rem' } }}>
                         Hungry? Let us handle it
                     </Typography>
                 </div>
@@ -17,5 +17,7 @@ const SearchBar = () => {
         </>
     )
 }
+// <Typography sx={{ zIndex: 1, color: 'white', fontSize: { xs: '1rem', sm: '1.5rem', md: '2.5rem' } }}>
+// <Typography sx={{ zIndex: 1, color: 'white', fontSize: { xs: '2rem', md: '3rem' } }}>
 
 export default SearchBar
