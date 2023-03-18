b812c673d8a5ac49e0555f99e82cc824f42b00d6
M17ggie <rmhatre799@gmail.com>
2023-03-19 00:07:28 +0530

    added validaion in login and signup forms

--- Frontend/src/components/login-signup/LoginForm.tsx
+++ Frontend/src/components/login-signup/LoginForm.tsx
@@ -1,13 +1,77 @@
-import React from 'react'
+import { useState } from 'react'
 import TextField from '@mui/material/TextField'
 import { Box, Button } from '@mui/material'
+import * as yup from 'yup';
 
 const LoginForm = () => {
+
+    const [loginData, setLoginData] = useState({
+        phoneNumber: "",
+        password: ""
+    });
+    const [errors, setErrors] = useState<any>({})
+
+    const schema = yup.object().shape({
+        phoneNumber: yup.string().trim().matches(/^([7-9]\d{9})$/, 'Please enter valid phone number'),
+        password: yup.string().required().trim().test('len', "Password must be between 8 & 15 characters", value => value.length >= 8 && value.length <= 15)
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setLoginData({
+            ...loginData,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            await schema.validate(loginData, { abortEarly: false });
+            setErrors({})
+            console.log(loginData)
+        } catch (err: unknown) {
+            const newErrors: Record<string, string> = {}
+            if (err instanceof yup.ValidationError) {
+                err.inner.forEach((error: any) => {
+                    newErrors[error.path] = error.message;
+                });
+                setErrors(newErrors)
+            } else {
+                console.log('What just happened is totally out of my scope')
+            }
+        }
+    }
+
     return (
-        <Box component='form' sx={{ display: 'flex', flexDirection: 'column', gap: 3 }} >
-            <TextField inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} size="small" variant='outlined' placeholder='Phone Number' />
-            <TextField type='password' size="small" variant='outlined' placeholder='Password' />
-            <Button variant='contained' sx={{ width: '100%', maxWidth: { lg: '25%' }, margin: '0 auto' }}>Login</Button>
+        <Box onSubmit={handleSubmit} component='form' sx={{ display: 'flex', flexDirection: 'column', gap: 3 }} >
+            <TextField
+                onChange={handleChange}
+                value={loginData.phoneNumber}
+                name="phoneNumber"
+                id="phoneNumber"
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                size="small"
+                variant='outlined'
+                placeholder='Phone Number'
+                error={!!errors.phoneNumber}
+                helperText={<span style={{ color: 'red' }}>{errors.phoneNumber}</span>}
+            />
+
+            <TextField
+                onChange={handleChange}
+                value={loginData.password}
+                name="password"
+                id="password"
+                type='password'
+                size="small"
+                variant='outlined'
+                placeholder='Password'
+                error={!!errors.phoneNumber}
+                helperText={<span style={{ color: 'red' }}>{errors.password}</span>}
+            />
+
+            <Button type='submit' variant='contained' sx={{ width: '100%', maxWidth: { lg: '25%' }, margin: '0 auto' }}>Login</Button>
         </Box>
     )
 }
--- Frontend/src/components/login-signup/SignUpForm.tsx
+++ Frontend/src/components/login-signup/SignUpForm.tsx
@@ -1,13 +1,88 @@
 import { Box, Button, TextField } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
+import * as yup from 'yup'
 
 const SignUpForm = () => {
+
+    const [signUpData, setSignUpData] = useState({
+        name: '',
+        phoneNumber: '',
+        password: ''
+    });
+
+    const [errors, setErrors] = useState<any>({});
+
+    const schema = yup.object().shape({
+        name: yup.string().min(3, 'Name must be atleast 3 characters long').max(20, 'Name should not exceed 20 characters').matches(/^[a-zA-Z\s]*$/, "Name must not include numbers or special characters").required('Name is a required field'),
+        phoneNumber: yup.string().required().matches(/^([7-9]\d{9})$/, 'Please enter valid phone number'),
+        password: yup.string().required().trim().test('len', "Password must be between 8 & 15 characters", value => value.length >= 8 && value.length <= 15)
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setSignUpData({
+            ...signUpData,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            await schema.validate(signUpData, { abortEarly: false });
+            setErrors({})
+            console.log(signUpData)
+        } catch (err: unknown) {
+            if (err instanceof yup.ValidationError) {
+                const newErrors: { [key: string]: string } = {}
+                err.inner.forEach((error: any) => {
+                    newErrors[error.path] = error.message
+                })
+                setErrors(newErrors);
+            } else {
+                console.log('This is beyond my scope')
+            }
+        }
+    }
+
     return (
-        <Box component='form' sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
-            <TextField type="text" size="small" variant='outlined' placeholder='Name' />
-            <TextField inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} size="small" variant='outlined' placeholder='Phone Number' />
-            <TextField type="password" size="small" variant='outlined' placeholder='Password' />
-            <Button variant='contained' sx={{ width: '100%', maxWidth: { lg: '25%' }, margin: '0 auto' }}>Sign Up!</Button>
+        <Box onSubmit={handleSubmit} component='form' sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
+            <TextField
+                onChange={handleChange}
+                type="text"
+                name="name"
+                id="name"
+                size="small"
+                variant='outlined'
+                placeholder='Name'
+                error={!!errors.name}
+                helperText={<span style={{ color: 'red' }}>{errors.name}</span>}
+            />
+
+            <TextField
+                onChange={handleChange}
+                inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+                size="small"
+                name="phoneNumber"
+                id="phoneNumber"
+                variant='outlined'
+                placeholder='Phone Number'
+                error={!!errors.phoneNumber}
+                helperText={<span style={{ color: 'red' }}>{errors.phoneNumber}</span>}
+            />
+
+            <TextField
+                onChange={handleChange}
+                type="password"
+                name="password"
+                id="password"
+                size="small"
+                variant='outlined'
+                placeholder='Password'
+                error={!!errors.password}
+                helperText={<span style={{ color: 'red' }}>{errors.password}</span>}
+            />
+            <Button type='submit' variant='contained' sx={{ width: '100%', maxWidth: { lg: '25%' }, margin: '0 auto' }}>Sign Up!</Button>
         </Box>
     )
 }
