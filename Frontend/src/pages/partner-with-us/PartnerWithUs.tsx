18ad922529afd02f682a4e495b0e4f838c88982f
M17ggie <rmhatre799@gmail.com>
2023-04-02 23:29:51 +0530

    Auth Modal added in Partner With Us page

--- Frontend/src/pages/partner-with-us/PartnerWithUs.tsx
+++ Frontend/src/pages/partner-with-us/PartnerWithUs.tsx
@@ -1,10 +1,16 @@
-import { Box, Card, Grid, Typography } from '@mui/material'
+import { Box, Card, Grid, Typography, Button } from '@mui/material'
 import styles from './PartnerWithUs.module.css'
 import CheckCircle from '@mui/icons-material/CheckCircle'
 import { HowItWorks, cardTitles, howItWorksData } from '@utils/texts/partner-with-us'
 import { CardContent } from '@material-ui/core'
+import AuthModal from '../../components/login-signup/AuthModal'
+import { useState } from 'react'
 
 const PartnerWithUs = () => {
+
+    const [openAuthModal, setOpenAuthModal] = useState(false);
+    const [showLogin, setShowLogin] = useState(true);
+    const [modalTitle, setModalTitle] = useState('')
 
     // document card+++++++++++++++
     const DocumentCardItem = ({ title }: { title: string }): JSX.Element => (
@@ -18,6 +24,22 @@ const PartnerWithUs = () => {
         </Box>
     )
 
+    const authModalHandler = () => {
+        setOpenAuthModal(false)
+    }
+
+    const loginContentHandler: Function = () => {
+        setOpenAuthModal(true);
+        setShowLogin(true);
+        setModalTitle('Login')
+    }
+
+    const signupContentHandler: Function = () => {
+        setOpenAuthModal(true);
+        setShowLogin(false);
+        setModalTitle('Sign Up')
+    }
+
     return (
         <>
             <div className="container">
@@ -30,6 +52,27 @@ const PartnerWithUs = () => {
                     <Typography sx={{ zIndex: 1, color: 'white', fontSize: { xs: '1rem', sm: '1.5rem', md: '2.5rem' } }}>
                         and get more customers!
                     </Typography>
+
+                    <Box sx={{
+                        display: "flex",
+                        marginTop: '1rem',
+                        flexDirection: { xs: 'column', md: 'row' },
+                    }}>
+                        <Button
+                            variant='contained'
+                            sx={{ marginRight: { xs: '0', md: '1rem' }, width: '25rem', padding: '1rem' }}
+                            onClick={() => { signupContentHandler() }}
+                        >
+                            Register with us
+                        </Button>
+                        <Button
+                            variant='contained'
+                            sx={{ backgroundColor: 'white', color: 'black', width: '25rem', marginTop: { xs: '1rem', md: '0' }, padding: '1rem' }}
+                            onClick={() => { loginContentHandler() }}
+                        >
+                            Already listed? Login now!
+                        </Button>
+                    </Box>
                 </div>
             </div >
 
@@ -75,12 +118,8 @@ const PartnerWithUs = () => {
             }}>
                 {
                     howItWorksData.map((element: HowItWorks, index: number) => (
-                        <Card variant='outlined' sx={{ width: '100%', margin: { xs:'1rem 0',md: '1rem 1rem' }, textAlign: 'center' }}>
-                            {/* <Box sx={{
-                                textAlign: 'center'
-                            }}> */}
+                        <Card key={index} variant='outlined' sx={{ width: '100%', margin: { xs: '1rem 0', md: '1rem 1rem' }, textAlign: 'center' }}>
                             {element.sprite}
-                            {/* </Box> */}
                             <CardContent>
                                 <Typography>
                                     {element.step}
@@ -96,6 +135,16 @@ const PartnerWithUs = () => {
                     ))
                 }
             </Box>
+
+            {/* auth modal****************** */}
+            <AuthModal
+                openAuthModal={openAuthModal}
+                authModalHandler={authModalHandler}
+                showLogin={showLogin}
+                modalTitle={modalTitle}
+                loginContentHandler={loginContentHandler}
+                signupContentHandler={signupContentHandler}
+            />
         </>
     )
 }
