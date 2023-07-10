ded2d8e7de97426893265aeb2a79dc0e6cb8578b
vistaar-rmhatre <raj.mhatre@vistaarmedia.com>
2023-07-10 17:58:51 +0530

    added temp protect middleware

---
+++ backend/src/middleware/protected.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { ErrorResponse } from "utils/errorResponse";
+
+export const adminOnly = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.cookies.token;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    if (decoded && decoded === "642b083d0fe5426488d3b5c9") {
+      next();
+    } else {
+      next(new ErrorResponse("Intruder", 401));
+    }
+  } catch (err) {}
+  //   if()
+};
